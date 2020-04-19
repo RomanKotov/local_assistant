c1abc5c6d27d7d466724e39b8ea63fccf661f556
Roman Kotov <RomanKotov@users.noreply.github.com>
2020-04-19 17:13:07 +0300

    Clean templates

--- test/local_assistant_web/live/page_live_test.exs
+++ test/local_assistant_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule LocalAssistantWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Welcome to Local Assistant!"
+    assert render(page_live) =~ "Welcome to Local Assistant!"
   end
 end
