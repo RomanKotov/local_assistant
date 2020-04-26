4f5d6e4ddd1dfad6c2ce438acb48ffa079d27e84
Roman Kotov <RomanKotov@users.noreply.github.com>
2020-04-26 22:08:14 +0300

    Update command line wrapper

--- priv/scripts/cmd_wrapper.js
+++ priv/scripts/cmd_wrapper.js
@@ -1,2 +1,14 @@
-process.stdin.on('data', console.log);
-process.stdin.on('end', () => process.exit(0));
+const {spawn} = require('child_process');
+
+const command = spawn('cat');
+
+command.on("close", code => process.exit(code));
+
+process.stdin.pipe(command.stdin);
+command.stdout.pipe(process.stdout);
+command.stderr.pipe(process.stderr);
+
+process.stdin.on('end', () => {
+  command.kill('SIGKILL');
+  process.exit(1);
+});
