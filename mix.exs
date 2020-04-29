8fba371bfb1b787d992edcb215e1443ad62603df
Roman Kotov <RomanKotov@users.noreply.github.com>
2020-04-29 17:22:23 +0300

    Update live dashboard

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule LocalAssistant.MixProject do
   def application do
     [
       mod: {LocalAssistant.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
@@ -38,7 +38,7 @@ defmodule LocalAssistant.MixProject do
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.1"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
